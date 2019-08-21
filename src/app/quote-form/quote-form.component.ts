8d0ae9e562858d5de009788a752dfa17befa5215
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-08-21 23:13:11 +0300

    Add functions for submitting quote and toggling details

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote=new Quote(0,"","","",new Date());
+  
+  @Output() addQuote=new EventEmitter<Quote>();
+  quotes: any;
+    
+  submitQuote(){
+      this.addQuote.emit(this.newQuote);
+      this.newQuote = new Quote(0,"","","",new Date());
+  }
+
+  toogleDetails(index){
+    this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+    this.quotes[index].showSubmit= !this.quotes[index].showSubmit;
+  }
 
   constructor() { }
 
